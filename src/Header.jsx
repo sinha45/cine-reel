3847cd9f25047953234a221214b3d94a457793ba
sinha45 <alfa45leonidas@gmail.com>
2024-11-14 23:53:26 +0600

    cart-details added

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,25 @@
+import { useState } from 'react';
+import Moon from "./assets/icons/moon.svg";
 import Logo from "./assets/logo.svg";
 import Ring from "./assets/ring.svg";
-import Moon from "./assets/icons/moon.svg";
-import Cart from "./assets/shopping-cart.svg"
+import Cart from "./assets/shopping-cart.svg";
+import CartDetails from './cine/CartDetails';
 
 export const Header = () => {
+
+    const [showCart, setShowCart] = useState(false);
+
+    function handleCartShow(){
+        setShowCart(true);
+    }
+
   return (
     <header>
+
+{
+showCart && <CartDetails onClose={() => setShowCart(false)}/>
+}
+
     <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
             <img src={Logo} width="139" height="26" alt="" />
@@ -23,7 +37,9 @@ export const Header = () => {
                 </a>
             </li>
             <li>
-                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#"
+                onClick={handleCartShow}
+                >
                     <img src={Cart} width="24" height="24" alt="cart" />
                 </a>
             </li>
